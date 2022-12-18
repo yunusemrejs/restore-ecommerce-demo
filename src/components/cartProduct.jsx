beb8bb156318bc059db2eb346a9bfe408dfaf5e4
Steve-bro <114855602+Steve-bro@users.noreply.github.com>
2022-12-18 17:13:56 +0300

    add/remove functionality added

--- src/components/cartProduct.jsx
+++ src/components/cartProduct.jsx
@@ -1,26 +1,36 @@
 import React from 'react'
 import './cartProduct.css'
 import {AiOutlinePlus} from 'react-icons/ai'
+import {FaMinus} from 'react-icons/fa'
+// import {GoPlus} from 'react-icons/go'
 
-export default function cartProduct({img, name, cartItems, price, id, remove, del}) {
+export default function cartProduct({img, name, cartItems, price, id, add, remove, del}) {
   return (
     <div className='cart-product-container'>
       <img src={img}/>
+
       <div className="product-details-wrapper">
         <div className="product-info">
-          <div className="text">
+          <div className="text">  
             <p className='title'>{name}</p>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
         </div>
+
         <div className="right-side-wrapper">
           <div className="price-options">
             <span>${price}</span>
-            <p>{cartItems}</p>
+            
+            <div className="quantity">
+              <button onClick={() => remove(id)}><FaMinus size={11}/></button>
+                <p>{cartItems}</p>
+              <button onClick={() => add(id)}><FaMinus size={11}/><FaMinus size={11}/></button>
+            </div>
           </div>
         
           <button onClick={() => del(id)} className='cancel-btn'><AiOutlinePlus size={17}/></button>
         </div>
+
       </div>
     </div> 
   )
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -7,18 +7,21 @@ export default function cart() {
   const { cartItems, addToCart, removeFromCart, deleteFromCart, cartCount, sumTotal} = useContext(ShopContext)
   // console.log(cartCount)
   return (
-    <div style={{width: '100%', height: '100%', display: 'flex', 
-      alignItems: 'center', flexDirection: 'column', gap: '20px'}}>
-      {Products.map((item) => 
-        {        
-          if (cartItems[item.id] !== 0) 
-            return (
-              <CartProduct key={item.id} id={item.id} img={item.ProductImage} name={item.ProductName} 
-                cartItems={cartItems[item.id]} price={item.ProductPrice} remove={removeFromCart} 
-                del={deleteFromCart}/> 
-            )      
-        }
-      )}
-    </div>
+    <>
+      <div style={{width: '100%', height: '100%', display: 'flex', 
+        alignItems: 'center', flexDirection: 'column', gap: '20px'}}>
+        {Products.map((item) => 
+          {        
+            if (cartItems[item.id] !== 0) 
+              return (
+                <CartProduct key={item.id} id={item.id} img={item.ProductImage} name={item.ProductName} 
+                  cartItems={cartItems[item.id]} price={item.ProductPrice} add={addToCart}
+                  remove={removeFromCart} del={deleteFromCart}/> 
+              )      
+          }
+        )}
+      </div>
+      
+    </>
   )
       }
