f41863e542a377b70ea3f40ca07053323f730a90
Steve-bro <114855602+Steve-bro@users.noreply.github.com>
2023-01-29 23:54:31 +0300

    credits

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -3,6 +3,8 @@ import Product from "../../components/product"
 import { useContext, useState } from "react"
 import { ShopContext } from "../../context/shopContext"
 import pay from '../../assets/paypal-logo-svgrepo-com.svg'
+import { SiReact } from "react-icons/si"
+import Heart from '../../assets/heart'
 import './shop.css'
 
 export default function shop() {
@@ -10,12 +12,23 @@ export default function shop() {
   const {newItems} = useContext(ShopContext)
 
   return (
-    <div className="product-container">
-      { 
-        newItems.map((item) => <Product newItems={newItems} key={item.id} data={item} 
-        setCartProducts={setCartProducts} cartProducts={cartProducts}/>)
-      }
-    </div>
+    <>
+      <div className="product-container">
+        { 
+          newItems.map((item) => <Product newItems={newItems} key={item.id} data={item} 
+          setCartProducts={setCartProducts} cartProducts={cartProducts}/>)
+        }
+      </div>
+      
+      <div className="credits">
+        made with
+        <Heart/> and
+        <a className="react" href="https://reactjs.org/"  target="_blank">
+        <SiReact color="#618ffb" size={'1.4em'} style={{marginInline: '3px'}}/>
+        </a> 
+        by<a className="name" href="https://github.com/Steve-bro" target="_blank">Steve</a>
+      </div>
+    </>
   )
 }
  
